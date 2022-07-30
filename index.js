a2a3ba2abe53d52df16b146210ff3ff64c405a39
mdatif796 <mdatif796@gmail.com>
2022-07-30 20:17:11 +0530

    Added static files to the server

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');   // requiring express for future use.
+const path = require('path');  // requiring path for attaching path with the root file 
 const port = 8000;     
 
-const app = express();   // creating app of express
 
+// creating app of express
+const app = express();   
+
+
+// adding static files 
+app.use(express.static('assets'));
+
+
+// setting up the view engine for ejs file
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', function(req, res){
+    res.render('home');
+});
 
 
 // my express app listening on this port
